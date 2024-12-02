ae79f49256da74d797f784797925f7db475379f2
terry1906 <azanov.egor1906@gmail.com>
2024-12-02 21:17:41 +0300

    new code

--- first.py
+++ first.py
@@ -1,10 +1,1 @@
-def main():
-    print('My first git program')
-    print('I have made some changes')
-    print("Hello")
-    print('This is a new feature')
-    print('I have made another change')
-
-
-if __name__ == '__main__':
-    main()
+print("My first git-repo")
